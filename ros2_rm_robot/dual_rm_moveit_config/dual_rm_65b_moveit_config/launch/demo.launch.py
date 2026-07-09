fa8cde1098cbbe0d24794a9a97c0215cee5eb3cf
Sampreeth Radha Krishna <rk.sampreeth@gmail.com>
2026-07-09 15:29:14 -0700

    Revert "replaced the gripper with 2 finger gripper, MoveIt in progress"

--- ros2_rm_robot/dual_rm_moveit_config/dual_rm_65b_moveit_config/launch/demo.launch.py
+++ ros2_rm_robot/dual_rm_moveit_config/dual_rm_65b_moveit_config/launch/demo.launch.py
@@ -95,7 +95,6 @@ def generate_launch_description():
         executable="move_group",
         output="screen",
         parameters=[
-            {'use_sim_time': True},
             robot_description,
             robot_description_semantic,
             kinematics_yaml,
@@ -116,7 +115,6 @@ def generate_launch_description():
         output="log",
         arguments=["-d", rviz_full_config],
         parameters=[
-            {'use_sim_time': True},
             robot_description,
             robot_description_semantic,
             ompl_planning_pipeline_config,
